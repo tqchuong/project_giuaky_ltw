7114bf8e4be00853352d4140b20efda201f70485
minhcuong04 <22130032@st.hcmuaf.edu.vn>
2024-10-29 21:14:21 +0700

    update banner & sreach

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log('Happy developing ✨')
+// Doi sang dinh dang tien VND
+function vnd(price) {
+    return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
+}
+
+// Click vùng ngoài sẽ tắt Popup
+modalContainer.forEach(item => {
+    item.addEventListener('click', closeModal);
+});
+
+modalBox.forEach(item => {
+    item.addEventListener('click', function (event) {
+        event.stopPropagation();
+    })
+});
+
+function closeModal() {
+    modalContainer.forEach(item => {
+        item.classList.remove('open');
+    });
+    console.log(modalContainer)
+    body.style.overflow = "auto";
+}
+
+
+
+// Open Search Advanced
+document.querySelector(".filter-btn").addEventListener("click",(e) => {
+    e.preventDefault();
+    document.querySelector(".advanced-search").classList.toggle("open");
+    document.getElementById("home-service").scrollIntoView();
+})
+
+document.querySelector(".form-search-input").addEventListener("click",(e) => {
+    e.preventDefault();
+    document.getElementById("home-service").scrollIntoView();
+})
+
+function closeSearchAdvanced() {
+    document.querySelector(".advanced-search").classList.toggle("open");
+}
+
+
+
+
+
+
+
+
+
+
+
