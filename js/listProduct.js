0d1a45e0a96810577b32fb8144aa1142cd63a576
minhcuong04 <22130032@st.hcmuaf.edu.vn>
2024-11-01 00:24:52 +0700

    update sanpham & phantrang

---
+++ js/listProduct.js
@@ -0,0 +1,66 @@
+function createProduct() {
+    localStorage.removeItem('products');
+    if (localStorage.getItem('products') == null) {
+        let product = [{
+            id: 1,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 2,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 3,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 4,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 5,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 6,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 7,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }, {
+            id: 8,
+            status: 1,
+            title: 'name-product',
+            img: 'src',
+            category: 'Phan loai',
+            price: 200000,
+        }
+        ]
+        localStorage.setItem('products', JSON.stringify(product));
+    }
+}
+
+window.onload = createProduct();
--- js/trang chủ.js
+++ js/trang chủ.js
@@ -18,6 +18,91 @@ document.querySelector(".form-search-input").addEventListener("click",(e) => {
 function closeSearchAdvanced() {
     document.querySelector(".advanced-search").classList.toggle("open");
 }
+// Page
+function renderProducts(showProduct) {
+    let productHtml = '';
+    if(showProduct.length == 0) {
+        document.getElementById("home-title").style.display = "none";
+        productHtml = `<div class="no-result"><div class="no-result-h">Tìm kiếm không có kết quả</div><div class="no-result-p">Xin lỗi, chúng tôi không thể tìm được kết quả hợp với tìm kiếm của bạn</div><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div></div>`;
+    } else {
+        document.getElementById("home-title").style.display = "block";
+        showProduct.forEach((product) => {
+            productHtml += `<div class="col-product">
+            <article class="card-product" >
+                <div class="card-header">
+                    <a href="#" class="card-image-link" onclick="detailProduct(${product.id})">
+                    <img class="card-image" src="${product.img}" alt="${product.title}">
+                    </a>
+                </div>
+                <div class="food-info">
+                    <div class="card-content">
+                        <div class="card-title">
+                            <a href="#" class="card-title-link" onclick="detailProduct(${product.id})">${product.title}</a>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="product-price">
+                            <span class="current-price">${vnd(product.price)}</span>
+                        </div>
+                    <div class="product-buy">
+                        <button onclick="detailProduct(${product.id})" class="card-button order-item"><i class="fa-regular fa-cart-shopping-fast"></i> Đặt món</button>
+                    </div> 
+                </div>
+                </div>
+            </article>
+        </div>`;
+        });
+    }
+    document.getElementById('home-products').innerHTML = productHtml;
+}
+
+// Phân trang
+let perPage = 4;
+let currentPage = 1;
+let totalPage = 0;
+let perProducts = [];
+
+function displayList(productAll, perPage, currentPage) {
+    let start = (currentPage - 1) * perPage;
+    let end = (currentPage - 1) * perPage + perPage;
+    let productShow = productAll.slice(start, end);
+    renderProducts(productShow);
+}
+
+function showHomeProduct(products) {
+    let productAll = products.filter(item => item.status == 1)
+    displayList(productAll, perPage, currentPage);
+    setupPagination(productAll, perPage, currentPage);
+}
+
+window.onload = showHomeProduct(JSON.parse(localStorage.getItem('products')))
+
+function setupPagination(productAll, perPage) {
+    document.querySelector('.page-nav-list').innerHTML = '';
+    let page_count = Math.ceil(productAll.length / perPage);
+    for (let i = 1; i <= page_count; i++) {
+        let li = paginationChange(i, productAll, currentPage);
+        document.querySelector('.page-nav-list').appendChild(li);
+    }
+}
+
+function paginationChange(page, productAll, currentPage) {
+    let node = document.createElement(`li`);
+    node.classList.add('page-nav-item');
+    node.innerHTML = `<a href="javascript:;">${page}</a>`;
+    if (currentPage == page) node.classList.add('active');
+    node.addEventListener('click', function () {
+        currentPage = page;
+        displayList(productAll, perPage, currentPage);
+        let t = document.querySelectorAll('.page-nav-item.active');
+        for (let i = 0; i < t.length; i++) {
+            t[i].classList.remove('active');
+        }
+        node.classList.add('active');
+        document.getElementById("home-service").scrollIntoView();
+    })
+    return node;
+}
 
 
 
