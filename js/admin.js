f3053333281bf59218184bbbbe74877d0d523232
Tu Quang Chuong <22130029@st.hcmuaf.edu.vn>
2024-11-01 00:04:22 +0700

    update admin nhung chua hoan thanh

---
+++ js/admin.js
@@ -0,0 +1,52 @@
+function detailOrderProduct(arr,id) {
+    let orderHtml = "";
+    arr.forEach(item => {
+        if(item.id == id) {
+            orderHtml += `<tr>
+            <td>${item.madon}</td>
+            <td>${item.quantity}</td>
+            <td>${vnd(item.price)}</td>
+            <td>${formatDate(item.time)}</td>
+            </tr>      
+            `;
+        }
+    });
+    document.getElementById("show-product-order-detail").innerHTML = orderHtml
+    document.querySelector(".modal.detail-order-product").classList.add("open")
+}
+
+function showProductArr(arr) {
+    let productHtml = "";
+    if(arr.length == 0) {
+        productHtml = `<div class="no-result"><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div><div class="no-result-h">Không có sản phẩm để hiển thị</div></div>`;
+    } else {
+        arr.forEach(product => {
+            let btnCtl = product.status == 1 ? 
+            `<button class="btn-delete" onclick="deleteProduct(${product.id})"><i class="fa-regular fa-trash"></i></button>` :
+            `<button class="btn-delete" onclick="changeStatusProduct(${product.id})"><i class="fa-regular fa-eye"></i></button>`;
+            productHtml += `
+            <div class="list">
+                    <div class="list-left">
+                    <img src="${product.img}" alt="">
+                    <div class="list-info">
+                        <h4>${product.title}</h4>
+                        <p class="list-note">${product.desc}</p>
+                        <span class="list-category">${product.category}</span>
+                    </div>
+                </div>
+                <div class="list-right">
+                    <div class="list-price">
+                    <span class="list-current-price">${vnd(product.price)}</span>                   
+                    </div>
+                    <div class="list-control">
+                    <div class="list-tool">
+                        <button class="btn-edit" onclick="editProduct(${product.id})"><i class="fa-light fa-pen-to-square"></i></button>
+                        ${btnCtl}
+                    </div>                       
+                </div>
+                </div> 
+            </div>`;
+        });
+    }
+    document.getElementById("show-product").innerHTML = productHtml;
+}
